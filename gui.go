3a00f2a99876131a7a3e6d4d20718576e94f8f85
Mendel Greenberg <chabad360@gmail.com>
2022-03-07 02:31:04 -0500

    add a gui

---
+++ gui.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/validation"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
+	driver "gitlab.com/gomidi/rtmididrv"
+	"runtime"
+)
+
+func gui() {
+	w := a.NewWindow("VMix to X-Touch")
+	//w.SetIcon(logoResource)
+
+	infoLabel := widget.NewRichTextWithText("Forwarding Off")
+	infoLabel.Wrapping = fyne.TextWrapOff
+
+	vmixPort := widget.NewEntry()
+	vmixPort.SetText(VMixPort)
+	vmixPort.Validator = validation.NewRegexp(`^[0-9]*$`, "not a valid port")
+
+	vmixAddr := widget.NewEntry()
+	vmixAddr.SetText(VMixAddr)
+	vmixAddr.Validator = validation.NewRegexp(`^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$`, "not a valid IP address")
+
+	ins, outs := insAndOuts(drv)
+
+	xtouchIn := widget.NewSelectEntry(ins)
+	xtouchIn.Validator = listValidator(ins)
+	xtouchOut := widget.NewSelectEntry(outs)
+	xtouchOut.Validator = listValidator(outs)
+
+	activator := widget.NewSelectEntry(ins)
+	activator.Validator = listValidator(ins)
+	shortcut := widget.NewSelectEntry(outs)
+	shortcut.Validator = listValidator(outs)
+
+	refreshButton := widget.NewButton("Refresh List", func() {
+		ins, outs = insAndOuts(drv)
+		xtouchIn.SetOptions(ins)
+		xtouchOut.SetOptions(outs)
+
+		activator.SetOptions(ins)
+		shortcut.SetOptions(outs)
+	})
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "VMix API Address", Widget: vmixAddr, HintText: "VMix API Address (this should be 127.0.0.1)"},
+			{Text: "VMix API Port", Widget: vmixPort, HintText: "VMix API Port (usually 8088)"},
+			{Text: "VMix Activator Loop Port", Widget: activator, HintText: "This should be a LoopMIDI port"},
+			{Text: "VMix Shortcut Loop Port", Widget: shortcut, HintText: "This should be a LoopMIDI port"},
+			{Text: "X-Touch Input Port", Widget: xtouchIn, HintText: "This should be called X-Touch 0"},
+			{Text: "X-Touch Output Port", Widget: xtouchOut, HintText: "This should be called X-Touch 0"},
+			{Text: "Refresh MIDI list", Widget: refreshButton, HintText: "Refresh the available MIDI ports"},
+		},
+		SubmitText: "Start Forwarding",
+		CancelText: "Stop Forwarding",
+	}
+
+	form.OnCancel = nil
+	form.OnSubmit = func() {
+		infoLabel.ParseMarkdown("Starting Forwarding")
+		running = false
+		wg.Wait()
+		VMixPort = vmixPort.Text
+		VMixAddr = vmixAddr.Text
+
+		i, err := drv.Ins()
+		if err != nil {
+			dialog.ShowError(err, w)
+			infoLabel.ParseMarkdown("Forwarding Errored")
+			return
+		}
+		o, err := drv.Outs()
+		if err != nil {
+			dialog.ShowError(err, w)
+			infoLabel.ParseMarkdown("Forwarding Errored")
+			return
+		}
+
+		if f, ok := getInList(activator.Text, ins); ok {
+			Activator = i[f]
+		} else {
+			infoLabel.ParseMarkdown("Forwarding Errored")
+			return
+		}
+		if f, ok := getInList(shortcut.Text, outs); ok {
+			Shortcut = o[f]
+		} else {
+			infoLabel.ParseMarkdown("Forwarding Errored")
+			return
+		}
+
+		if f, ok := getInList(xtouchIn.Text, ins); ok {
+			XTouchIn = i[f]
+		} else {
+			infoLabel.ParseMarkdown("Forwarding Errored")
+			return
+		}
+		if f, ok := getInList(xtouchOut.Text, outs); ok {
+			XTouchOut = o[f]
+		} else {
+			infoLabel.ParseMarkdown("Forwarding Errored")
+			return
+		}
+
+		running = true
+
+		if err = startMidi(); err != nil {
+			dialog.ShowError(err, w)
+			infoLabel.ParseMarkdown("Forwarding Errored")
+			return
+		}
+
+		infoLabel.ParseMarkdown(fmt.Sprintf("Forwarding messages."))
+		form.SubmitText = "Update Server"
+
+		form.OnCancel = func() {
+			running = false
+			wg.Wait()
+
+			form.OnCancel = nil
+			form.SubmitText = "Start Forwarding"
+
+			form.Refresh()
+			runtime.GC()
+		}
+
+		form.Refresh()
+		runtime.GC()
+	}
+
+	w.SetContent(container.NewGridWithRows(2, form, infoLabel))
+	w.ShowAndRun()
+}
+
+func insAndOuts(d *driver.Driver) (i []string, o []string) {
+	ins, _ := d.Ins()
+	outs, _ := d.Outs()
+	i = make([]string, len(ins))
+	for f := 0; f < len(ins); f++ {
+		i[f] = ins[f].String()
+	}
+	o = make([]string, len(outs))
+	for f := 0; f < len(outs); f++ {
+		o[f] = outs[f].String()
+	}
+	return
+}
+
+func getInList(item string, items []string) (int, bool) {
+	for i := 0; i < len(items); i++ {
+		if items[i] == item {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+func listValidator(items []string) func(string) error {
+	return func(s string) error {
+		if _, ok := getInList(s, items); !ok {
+			return fmt.Errorf("invalid entry")
+		}
+		return nil
+	}
+}
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
-	"time"
-
+	"fyne.io/fyne/v2/app"
+	vmix "github.com/FlowingSPDG/vmix-go/http"
 	xtouch "github.com/chabad360/goxtouch"
 	"gitlab.com/gomidi/midi"
 	"gitlab.com/gomidi/midi/reader"
+	"strconv"
+	"sync"
+	"time"
 
-	vmix "github.com/FlowingSPDG/vmix-go/http"
 	"gitlab.com/gomidi/midi/writer"
 	driver "gitlab.com/gomidi/rtmididrv"
 )
@@ -18,6 +17,20 @@ import (
 var (
 	wr  *writer.Writer
 	wr2 *writer.Writer
+
+	a   = app.NewWithID("me.chabad360.vmix-to-xtouch")
+	drv *driver.Driver
+
+	VMixPort = "8088"
+	VMixAddr = "127.0.0.1"
+
+	Activator midi.In
+	Shortcut  midi.Out
+	XTouchIn  midi.In
+	XTouchOut midi.Out
+
+	running = false
+	wg      = new(sync.WaitGroup)
 )
 
 func must(err error) {
@@ -29,70 +42,86 @@ func must(err error) {
 const color = byte(xtouch.ColorWhite + xtouch.InvertLine1 + xtouch.InvertLine2)
 const interval = float64(2) / 127
 
-// This example demonstrates using github.com/chabad360/goxtouch to send and receive signals using the CTRL mode interface for the Behringer X-Touch Universal
 func main() {
-	drv, err := driver.New()
-	must(err)
-
-	// make sure to close all open ports at the end
+	drv, _ = driver.New()
 	defer drv.Close()
+	gui()
+}
 
-	ins, err := drv.Ins()
-	must(err)
+func startMidi() error {
+	if err := Activator.Open(); err != nil {
+		return err
+	}
+	if err := Shortcut.Open(); err != nil {
+		return err
+	}
+	if err := XTouchOut.Open(); err != nil {
+		return err
+	}
+	if err := XTouchIn.Open(); err != nil {
+		return err
+	}
 
-	outs, err := drv.Outs()
-	must(err)
-
-	fmt.Println(ins, outs)
-
-	in, out := ins[0], outs[1]
-	in2, out2 := ins[2], outs[4]
-
-	must(in.Open())
-	must(out.Open())
-	must(in2.Open())
-	must(out2.Open())
-
-	wr = writer.New(out)
+	wr = writer.New(XTouchOut)
 	wr.ConsolidateNotes(false)
-	wr2 = writer.New(out2)
+	wr2 = writer.New(Shortcut)
 	wr2.ConsolidateNotes(false)
 	rd := reader.New(reader.Each(forwardFrom), reader.NoLogger())
 	rd2 := reader.New(reader.Each(forwardto), reader.NoLogger())
 
-	go rd.ListenTo(in)
-	go rd2.ListenTo(in2)
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		rd.ListenTo(XTouchIn)
+	}()
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		rd2.ListenTo(Activator)
+	}()
 
-	http.Get("http://localhost:8088/API?Function=SetBusGVolume&Value=0")
+	//http.Get("http://localhost:8088/API?Function=SetBusGVolume&Value=0")
 
-	c, err := vmix.NewClient("localhost", 8088)
-	must(err)
-
-	for {
-		total := 6
-		if len(c.Inputs.Input) < 6 {
-			total = len(c.Inputs.Input)
-		}
-		for i := 0; i < total; i++ {
-			wr.Write(xtouch.SetLCD(xtouch.Channel1+xtouch.Channel(i), color, strconv.Itoa(i+1), c.Inputs.Input[i].Name))
-			b := c.Inputs.Input[i].Balance + 1
-			wr.Write(xtouch.SetEncoder(xtouch.Channel1+xtouch.Channel(i), uint8(b/interval)))
-		}
-		if t := 6 - total; t > 0 {
-			for i := t - 1; i >= 0; i-- {
-				wr.Write(xtouch.SetLCD(xtouch.Channel6-xtouch.Channel(i), byte(xtouch.ColorNone), strconv.Itoa(i+1), ""))
-			}
-		}
-		wr.Write(xtouch.SetLCD(xtouch.Channel7, byte(xtouch.ColorYellow+48), "Preview", c.Inputs.Input[c.Preview-1].Name))
-		b := c.Inputs.Input[c.Preview-1].Balance + 1
-		wr.Write(xtouch.SetEncoder(xtouch.Channel7, uint8(b/interval)))
-		wr.Write(xtouch.SetLCD(xtouch.Channel8, byte(xtouch.ColorGreen+48), "Active", c.Inputs.Input[c.Active-1].Name))
-		b = c.Inputs.Input[c.Active-1].Balance + 1
-		wr.Write(xtouch.SetEncoder(xtouch.Channel8, uint8(b/interval)))
-		time.Sleep(time.Millisecond * 20)
-		c, err = vmix.NewClient("localhost", 8088)
-		must(err)
+	p, _ := strconv.Atoi(VMixPort)
+	c, err := vmix.NewClient(VMixAddr, p)
+	if err != nil {
+		return err
 	}
+
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		for running {
+			total := 6
+			if len(c.Inputs.Input) < 6 {
+				total = len(c.Inputs.Input)
+			}
+			for i := 0; i < total; i++ {
+				wr.Write(xtouch.SetLCD(xtouch.Channel1+xtouch.Channel(i), color, strconv.Itoa(i+1), c.Inputs.Input[i].Name))
+				b := c.Inputs.Input[i].Balance + 1
+				wr.Write(xtouch.SetEncoder(xtouch.Channel1+xtouch.Channel(i), uint8(b/interval)))
+			}
+			if t := 6 - total; t > 0 {
+				for i := t - 1; i >= 0; i-- {
+					wr.Write(xtouch.SetLCD(xtouch.Channel6-xtouch.Channel(i), byte(xtouch.ColorNone), strconv.Itoa(i+1), ""))
+				}
+			}
+			wr.Write(xtouch.SetLCD(xtouch.Channel7, byte(xtouch.ColorYellow+48), "Preview", c.Inputs.Input[c.Preview-1].Name))
+			b := c.Inputs.Input[c.Preview-1].Balance + 1
+			wr.Write(xtouch.SetEncoder(xtouch.Channel7, uint8(b/interval)))
+			wr.Write(xtouch.SetLCD(xtouch.Channel8, byte(xtouch.ColorGreen+48), "Active", c.Inputs.Input[c.Active-1].Name))
+			b = c.Inputs.Input[c.Active-1].Balance + 1
+			wr.Write(xtouch.SetEncoder(xtouch.Channel8, uint8(b/interval)))
+			time.Sleep(time.Millisecond * 20)
+			p, _ = strconv.Atoi(VMixPort)
+			c, err = vmix.NewClient(VMixAddr, p)
+			must(err)
+		}
+		drv.Close()
+		drv, _ = driver.New()
+	}()
+
+	return nil
 }
 
 func forwardto(p *reader.Position, m midi.Message) {
